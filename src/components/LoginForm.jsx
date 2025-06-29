2820ea9fd90322132cd9eecb5e3f57c7852f72c4
unknown <nimmanasathish31@gmail.com>
2025-06-29 21:49:02 +0530

    functionality added

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -72,7 +72,7 @@ function LoginForm() {
         )}
 
         <div>
-          <label className="block text-gray-600 mb-1">Email</label>
+          <label  className="block text-gray-600 mb-1">Email</label>
           <input
             type="email"
             className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
